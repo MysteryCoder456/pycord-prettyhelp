7873b6cf79e0dc0ac71ab371c4cbcc06d3cfe8c8
Rehatbir Singh <rehatbir.singh@gmail.com>
2021-12-12 19:24:35 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="discord-ext-prettyhelp",
-    author="Circuit",
-    url="https://github.com/CircuitsBots/discord-ext-prettyhelp",
+    name="pycord-prettyhelp",
+    author="CodeBoi",
+    url="https://github.com/MysteryCoder456/pycord-prettyhelp",
     version=__version__,
     packages=["discord.ext.prettyhelp"],
     license="MIT",
     description="A pretty embeded version of the default help command.",
     long_description=README,
     long_description_content_type="text/markdown",
-    install_requires=["discord.py>=1.2.5"],
+    install_requires=["py-cord"],
     python_requires=">=3.5.3",
 )
